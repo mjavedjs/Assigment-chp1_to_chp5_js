d3918db5fd78919029fd1699f69c8fea0bee776e
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-08-29 11:59:49 +0500

    chp4 and chp_5 solution

---
+++ app.js
@@ -0,0 +1,136 @@
+// legal variable name
+// var name="javed"
+// var Father_Name="javed"
+// var id=123;
+// var result;
+// var class_name;
+ 
+// illegal Name of varibel;
+// var @name="wali";
+// var 124fathername="";
+// var +class_name="";
+// var name class=""
+// var id()="erere"
+
+
+// document.write("The varible only contian number  $ and _ underscore ForExample:$my_1stVarible  <br>");
+// document.write("varible must start with letter or $  ForExample S_name or  name  <br> ");
+// document.write("varible are case Sentitive  <br>");
+// document.write("varible name should not be js keywords  <br>");
+// sum of two number
+// var number1= +prompt("enter number");
+// var number2= +prompt("enter number");
+// let result= number1 + number2;
+// console.log("The Sum of"   + number1  + "and" +  number2   + "is"     + result );
+
+// var number1= +prompt("enter number");
+// var number2= +prompt("enter number");
+// let result2= number1 - number2;
+// console.log("The Subrcation of"   + number1  + "and" +  number2   + "is"     + result2 );
+
+// var number1= +prompt("enter number");
+// var number2= +prompt("enter number");
+// let result3= number1 * number2;
+// console.log("The Multplication of"   + number1  + "and" +  number2   + "is"     + result3 );
+//  
+        //  Q4
+// var ticket_Cost=600;
+// var total_ticket=600*5;
+// console.log( "The Price of Five ticket is " + total_ticket);
+//   Q5  Table by using script 
+// var number1 = +prompt("enter fist number");
+// var number2 = +prompt("enter first number");
+// var number3 = +prompt("enter fist number");
+// var number4 = +prompt("enter fist number");
+// var number5 = +prompt("enter fist number");
+// var number6 = +prompt("enter fist number");
+// var number7= +prompt("enter fist number");
+// var number8 = +prompt("enter fist number");
+// var number9 = +prompt("enter fist number");
+// var number10 = +prompt("enter fist number");
+// var result1 = number1*1;
+// var result2 = number2*2;
+// var result3 = number3*3;
+// var result4 = number4*4;
+// var result5 = number5*5;
+// var result6 = number6*6;
+// var result7 = number7*7;
+// var result8 = number8*8;
+// var result9 = number9*9;
+// var result10 = number10*10;
+// console.log( "4 x = "+result1);
+// console.log( "4 x = " + result2 );
+// console.log( "4 x = " + result3 );
+// console.log( "4 x = " + result4 );
+// console.log( "4 x = " + result5 );
+// console.log( "4 x = " + result6 );
+// console.log( "4 x = " + result7 );
+// console.log( "4 x = " + result8 );
+// console.log( "4 x = " + result9 );
+// console.log( "4 x = " + result10); 
+//        Q6
+// var myVariable="?";
+// console.log("The value of varilbe before declartion"+myVariable);
+// myVariable =5;
+// console.log("The inital value of varible is " + myVariable);
+// myVariable ++;
+// console.log("The value after inncrement is " + myVariable)
+// myVariable +=7;
+// console.log("after the additonn value  " + myVariable);
+// myVariable --;
+// console.log("Afer the dec value is "  + myVariable);
+// myVariable= myVariable % 3;
+// console.log("The reminder value of my variable is " +myVariable);
+//     Q7 shopping cart
+//  var price_of_item1=650;
+//  var price_of_item2=100;
+//  var Quantity_item1=3;
+//  var Quantity_item2=7;
+//  var shipping_Charge=100;
+
+//  console.log("The price of itme 1 is  "+ price_of_item1);
+//  console.log("The price of itmee 2 is  "+ price_of_item2);
+//  console.log("The Quantity of item 1  "+ Quantity_item1);
+//  console.log("The Quantity of item 2  "+ Quantity_item2);
+//  var prodcut1=price_of_item1*Quantity_item1;
+//  var prodcut2=price_of_item2*Quantity_item2;
+//  var result = prodcut1+prodcut2+shipping_Charge;
+//  console.log("The total cost of you order  " +result)
+
+
+        //    Q8 student percentage;
+        // var total=980;
+        // var obtained=804;
+        // var percantage=( obtained/total)*100;
+        // var perFormte=percantage.toFixed(2)
+        // console.log(perFormte+" %")
+
+        // Q9
+
+// var usd1=280;
+// var saudi=70;
+// var p=usd1*10;
+// var p2=saudi*25;
+// var result=p+p2;
+// console.log("The Total Currency in PKR  "+result +" Ruppes")
+//              Q10
+// var number =10;
+// let result=(( number + 5) *10 ) /2;
+// console.log(result)
+
+// Q11
+
+// var areaofCricel= "Circle";
+// var radius=20;
+// var result=3.142 *(radius*radius);
+// console.log( "The area of circel is  " + result  );
+
+//                   Q12
+
+// var currentAge = 18;
+// var maxAge = 60;
+// var amountOfChipsPerDay = 3;
+// var daysInYear = 365;
+// var totalChipsNeeded = (maxAge - currentAge) * daysInYear * amountOfChipsPerDay;
+// console.log(totalChipsNeeded);
+
